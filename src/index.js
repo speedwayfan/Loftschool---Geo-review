576375933e08f1ae26ce66e379c2d14bbe28250c
speedwayfan <speedwayfan@mail.ru>
2018-06-10 23:57:57 +0300

    fourth version hw

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-        const points = [];
-        // const item = {};
-        const reviews = [];
+        // const points = [];
+        // // const item = {};
+        // const reviews = [];
         // const newRewiewObj = {};
+    // const modal = document.querySelector(".modal");
+    // const modalTitle = document.querySelector(".modal__title");
+    // const modalReviews = document.querySelector(".modal__reviews");
+    // const inputName = document.querySelector(".input__name");
+    // const inputPlace = document.querySelector(".input__place");
+    // const inputArea = document.querySelector(".input__area");
+    // const addButton = document.querySelector(".add__button");
+    // const closeModal = document.querySelector(".smallCross");
+    // const clientWidth = document.body.clientWidth;
+    // const clientHeight = document.body.clientHeight;
 
 ymaps.ready(function () {
-    // var mapCenter = [55.755381, 37.619044],
        var map = new ymaps.Map('map', {
             center: [55.755381, 37.619044],
             zoom: 11,
@@ -15,8 +24,9 @@ ymaps.ready(function () {
     var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
         // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
         '<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
-            '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
-            '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
+        '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
+        '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>' + 
+        '<div class=date_footer>{{ properties.balloonContentDate|raw }}</div>'
     );
 
     var clusterer = new ymaps.Clusterer({
@@ -71,7 +81,7 @@ ymaps.ready(function () {
                 modalReviews.innerText = 'Отзывов пока нет...';        // это наверно не обязательно, по новому клику всегда новое окно открывается
             });
 
-            // если модальное окно display:none, то вызываем функцию открытия и задаем координаты верхней левой точки
+            // если модальное окно display:none, то вызываем функцию открытия
             if (modal.classList.contains('modal-closed')) {
                 if (y > clientHeight - 425) {
                     if (x > clientWidth - 280) {
@@ -101,26 +111,27 @@ ymaps.ready(function () {
 
             addButton.addEventListener('click', () => {
                 if (inputName.value != '' || inputPlace.value != '' || inputArea.value != '') {
-                    // после клика добавить очищаем поле отзыва от шаблонной фразы и вызываем функцию создания блока отзыва
+                    // после клика 'добавить' очищаем поле отзыва от шаблонной фразы и вызываем функцию создания блока отзыва
                     modalReviews.innerText = '';
 
-                    createReview();
-                    createMark();
+                    createReview(adress);
                 }
             })
-        }
+        };
 
-        function createReview() {
+        function createReview(adress) {
             const divOutReview = document.createElement('div');
             const divNamePlace = document.createElement('div');
             const divReview = document.createElement('div');
             const newDate = new Date();
 
-            divNamePlace.innerHTML = `${inputName.value} ${inputPlace.value} ${newDate.toLocaleString()};`
-            divReview.innerHTML = inputArea.value;
+            divNamePlace.innerHTML = `${inputName.value} ${inputPlace.value} ${newDate.toLocaleString()}`;
+            divReview.innerHTML = `${inputArea.value}`;
 
-            // добавляем инфо из инпута в блоки и сохраняем в объект эти значения с помощью функции
-            saveReview(divNamePlace.innerHTML, divReview.innerHTML);
+            const review = `${inputName.value} ${inputPlace.value} ${newDate.toLocaleString()} ${inputArea.value}`;
+
+            createMark(coords, adress, newDate.toLocaleString());
+            saveReview(coords, review);
 
             divOutReview.appendChild(divNamePlace);
             divOutReview.appendChild(divReview);
@@ -129,65 +140,62 @@ ymaps.ready(function () {
             inputName.value = '';
             inputPlace.value = '';
             inputArea.value = '';
-        }
+        };
 
-        function saveReview(namePlace, review) {
-            reviews.push({ divNamePlace : namePlace, divReview : review });
-            points.push({ id : coords, reviews : [reviews] });
+        function saveReview(coords, review) {
+            let points = [];
+            let reviews = [review];
 
-            localStorage.data = JSON.stringify({points});
-                console.log(localStorage.data);
-        }
+            if (localStorage.points) {
+                try {
+                    let parsedPoints = JSON.parse(localStorage.points);
+                    // Метод Array.isArray() возвращает true, если объект является массивом
+                    if (Array.isArray(parsedPoints)) {
+                        points = [...parsedPoints];
+                    } 
+                }
+                catch (e) {
+                    console.log(e.message);
+                }
+            };
+        // findIndex() возвращает индекс в массиве, если элемент удовлетворяет условию проверяющей функции, в противном случае: -1
+            const pointIndex = points.findIndex(item => item.id[0] == coords[0] && item.id[1] == coords[1]);
 
-    // const points = JSON.parse(localStorage.points);
-    // const review = { text: 'текст нового отзыва'}
-    // const index = points.findIndex(item => item.id == address) // находим точку с таким же адресом
-    // if(index !== -1){
-    //     const newPointState = {...points[index], reviews: [...points[index].reviews, review};
-    //     points.splice(index, newPointState);
-            // splice - удаляет часть массива и добавляет новые элементы на место удаленных
-    // } else {
-    //     points.push({ id: 'address', reviews: [review] })
-    // }
-    // localStorage.setItem('points', points);
+                if (pointIndex == -1) {
+                    reviews = [...reviews, ...points[pointIndex].reviews];        // выдает ошибку на-  .reviews]
+                    const point = { id: coords, reviews };
 
-        function createMark() {
-            // Заполняем кластер геообъектами
-            var placemarks = [];
-            // for (var i = 0, l = 10; i < l; i++) {
-            var placemark = new ymaps.Placemark(coords, {
-                // Устаналиваем данные, которые будут отображаться в балуне.
-                balloonContentHeader: 'inputPlace',
-                balloonContentBody: 'address - ссылка',
-                balloonContentFooter: 'inputArea'
-            }, {
-                preset: 'islands#icon',
-                iconColor: '#735184'
-            });
-            placemarks.push(placemark);
-            // }
-            clusterer.add(placemarks);
-            map.geoObjects.add(clusterer);
-        }
-    })
-    clusterer.balloon.open(clusterer.getClusters());
-});
-
-
-
-            //     // const points = [...localStorage.points];
-            //     // const point = points.findIndex(item => item.id[0] == coords[0] && item.id[1] == coords[1])
-
-            //     // if(point === -1){
-            //         // выполняем мой код
-            //     //     points.push({ id : coords, reviews : [reviews] });
-            //     // } else {
-            //     //     points[point].reviews.push(review)
-            //     // }
-            //     // localStorage.points = points;
+                    points.splice(pointIndex, point, 1);
+                } else {
+                    points.push({ id : coords, reviews })
+                }
+                localStorage.points = JSON.stringify(points);
 
             // reviews.push({ divNamePlace : namePlace, divReview : review });
             // points.push({ id : coords, reviews : [reviews] });
 
             // localStorage.data = JSON.stringify({points});
             //     console.log(localStorage.data);
+        };
+
+
+        function createMark(coords, adress, date) {
+            // Заполняем кластер геообъектами
+            var placemarks = [];
+            var placemark = new ymaps.Placemark(coords, {
+                // Устаналиваем данные, которые будут отображаться в балуне
+                balloonContentHeader: inputPlace.value,
+                balloonContentBody: adress,
+                balloonContentFooter: inputArea.value,
+                balloonContentDate: date
+            }, {
+                preset: 'islands#icon',
+                iconColor: '#735184'
+            });
+            placemarks.push(placemark);
+            clusterer.add(placemarks);
+            map.geoObjects.add(clusterer);
+        }
+    })
+    clusterer.balloon.open(clusterer.getClusters());
+})
